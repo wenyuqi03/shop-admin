81219389e0a0a2e818bc9a53ce1de82e357f3115
wyq914926182@163.com <wyq914926182@163.com>
2023-07-27 16:53:29 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:vue/vue3-strongly-recommended "
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": ["./tsconfig.json"],
+        "parser": '@typescript-eslint/parser',
+        "extraFileExtensions": [".vue"]
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
